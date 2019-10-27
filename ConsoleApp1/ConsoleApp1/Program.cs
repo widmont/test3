69a2343847787d53a93e0946c8eb4aaf6aef2c1e
antoine.widmer <antoine.widmer@hevs.ch>
2019-10-27 13:13:06 +0100

    commit in branch develop

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("second commit");
+            Console.WriteLine("only on branch develop");
         }
     }
 }
